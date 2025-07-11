69f33050a5a89af8b29eac1246917ffddb9215f0
dhanshree304 <dhanshreeupadhye0@gmail.com>
2025-07-11 14:29:00 +0530

    first commit

---
+++ typescript-app/playwrighttesting/src/App.tsx
@@ -0,0 +1,10 @@
+
+import "./App.css";
+import { Button } from "@/components/ui/button";
+function App() {
+  return <>
+  <Button className="text-4xl">Add</Button>
+  </>;
+}
+
+export default App;
---
+++ typescript-app/todoapp/src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Todos from './todos'
+
+
+function App() {
+
+
+  return (
+    <>
+     
+     <Todos/>   
+    </>
+  )
+}
+
+export default App
